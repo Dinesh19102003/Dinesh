ac8653214d0a9c9222b66833a9bbc453a82b92bd
Deeksha04102003 <dishacronical0blr@gmail.com>
2023-02-06 11:46:28 +0530

    My second commit

---
+++ Dineshh.java
@@ -0,0 +1,8 @@
+Public Class Subtraction
+     Public static void main(string[] args)
+     {
+        int a=6;
+        int b=3;
+        int Difference = a - b ;
+        system.out.println("Subtration is" +Difference);
+     }
